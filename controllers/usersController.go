ae57fd6f0d68aad502dd2111bdbe6bbd3e015c48
Espen Carlsen <espen.carlsen@outlook.com>
2024-01-10 17:36:12 +0000

    added the validate controller with no middleware

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -118,3 +118,9 @@ func Login(c *gin.Context) {
 	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
 	c.JSON(http.StatusOK, gin.H{})
 }
+
+func Validate(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "I'm logged in",
+	})
+}
