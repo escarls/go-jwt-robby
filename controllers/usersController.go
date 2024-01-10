6bebc93571d268c3295e81cc2240222c22570e93
Espen Carlsen <espen.carlsen@outlook.com>
2024-01-10 18:10:06 +0000

    validate will print out the whole user record

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -120,7 +120,8 @@ func Login(c *gin.Context) {
 }
 
 func Validate(c *gin.Context) {
+	user, _ := c.Get("user")
 	c.JSON(http.StatusOK, gin.H{
-		"message": "I'm logged in",
+		"message": user,
 	})
 }
